d951dc2d226a9db97a968a34db5f9c1a7adb3254
DuyetDS <duyetds@ariainc.vn>
2024-04-02 22:45:15 +0700

    update remove about page

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -20,7 +20,7 @@ export default function Header(props: HeaderProps) {
         <nav className="pointer-events-auto hidden md:block">
           <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
             <li>
-              <Link href="/about" legacyBehavior>
+              <Link href="/" legacyBehavior>
                 <a className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400 ">
                   About
                 </a>
