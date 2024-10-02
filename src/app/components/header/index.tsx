60e34be41cd22683c1c52cdea49dc17cd7b9ab41
duyetqb <duyetqb@gmail.com>
2024-10-02 20:33:40 +0700

    fix header in mobile

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -14,14 +14,14 @@ export default function Header(props: HeaderProps) {
   const { toggle, dark } = useContext(ThemeContext);
 
   return (
-    <header className="sticky top-0 z-10 h-16 pt-6 mx-auto max-w-2xl lg:max-w-5xl">
+    <header className="sticky top-0 z-100 h-16 pt-6 mx-auto max-w-2xl lg:max-w-5xl">
       <div className="relative flex gap-4 ">
         <div className="flex flex-1">
           {isAvatarHidden ? null : (
             <Avatar size={!isAvatarHidden ? "small" : "middle"} />
           )}
         </div>
-        <nav className="pointer-events-auto hidden md:block">
+        <nav className="pointer-events-auto md:block z-100">
           <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
             <li>
               <Link href="/" legacyBehavior>
