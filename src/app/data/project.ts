9612555ca1e10f78d3e2517acf309f562ceedd5c
duyetqb <69998574+DuyetQB@users.noreply.github.com>
2025-03-16 10:28:14 +0700

    add genteach product

--- src/app/data/project.ts
+++ src/app/data/project.ts
@@ -1,58 +1,74 @@
 export const projects = [
-    {
-        id: 1,
-        name: 'Driving theory UK',
-        description: 'Creating technologies that empower regular people to users with a comprehensive and user-friendly platform to enhance their knowledge and skills in preparation for the theory test.',
-        image: '',
-        type: 'App Store',
-        link: 'https://apps.apple.com/us/app/driving-theory-ultimate-kit-uk/id6480314162?platform=iphone'
-    },
-    {
-        id: 2,
-        name: 'Files Convert',
-        description: 'FilesConvert is a versatile platform designed to streamline file type conversions. Whether you need to convert documents, images, audio, or video files, FilesConvert offers a seamless and efficient solution. With an intuitive interface and robust functionality, users can easily transform their files into the desired formats with just a few clicks',
-        image: '',
-        type: 'Website',
-        link: 'https://www.filesconvert.org/'
-    },
-    {
-        id: 3,
-        name: 'CrowdWork',
-        description: 'CrowdWork platform was developed with the mission of creating a strong community of freelancers everywhere in the world, regardless of time and distance in many different fields such as data entry.',
-        image: '',
-        type: 'Website',
-        link: 'https://crowdwork.dps-fpt.vn/home'
-    },
-    {
-        id: 4,
-        name: 'Money Buddy',
-        description: 'Money Buddy is your personal financial companion designed to help you effortlessly manage your finances. Whether you are tracking your daily expenses, monitoring your income, or setting savings goals, Money Buddy makes financial management simple and intuitive. With an easy-to-use interface and powerful features, you can categorize your spending, visualize your financial habits, and gain insights into your money flow.',
-        image: '',
-        type: 'CH-Play',
-        link: 'https://play.google.com/store/apps/details?id=com.lenodev.studio.moneybuddy'
-    },
-    {
-        id: 5,
-        name: 'Lenodev',
-        description: 'Lenodev is just a blog. Where I can share the technical blog for other developers.And many usefull tools I buid and annoumence in here',
-        image: '',
-        type: 'Website',
-        link: 'https://lenodev.com/blog'
-    },
-    {
-        id: 6,
-        name: 'React Server',
-        description: 'React server is a website build and using only Reactjs. It is a difficult technique. Dynamic metatag serves SEO. With many years of experience research and failed. Then until now I build it success. I have to deep understanding of SPA and SSR rendering mechanism then I can do it.',
-        image: '',
-        type: 'Website',
-        link: 'https://react-server-lqq0.onrender.com/'
-    },
-    {
-        id: 7,
-        name: 'Prismtouch',
-        description: 'A best performance UI library for React Native. It solve many problem of React native UI library. It is continue of building',
-        image: '',
-        type: 'Package',
-        link: 'https://www.npmjs.com/package/prismtouch'
-    },
-]
+  {
+    id: 1,
+    name: "Driving theory UK",
+    description:
+      "Creating technologies that empower regular people to users with a comprehensive and user-friendly platform to enhance their knowledge and skills in preparation for the theory test.",
+    image: "",
+    type: "App Store",
+    link: "https://apps.apple.com/us/app/driving-theory-ultimate-kit-uk/id6480314162?platform=iphone",
+  },
+  {
+    id: 1,
+    name: "Genteach",
+    description:
+      "GenTeach is a pioneering app for sex education, tailored specifically for teenagers and young adults aged 14-25. With rich, scientifically accurate, and age-appropriate content",
+    image: "",
+    type: "CH-Play",
+    link: "https://play.google.com/store/apps/details?id=com.genteach.education",
+  },
+  {
+    id: 2,
+    name: "Files Convert",
+    description:
+      "FilesConvert is a versatile platform designed to streamline file type conversions. Whether you need to convert documents, images, audio, or video files, FilesConvert offers a seamless and efficient solution. With an intuitive interface and robust functionality, users can easily transform their files into the desired formats with just a few clicks",
+    image: "",
+    type: "Website",
+    link: "https://www.filesconvert.org/",
+  },
+  {
+    id: 3,
+    name: "CrowdWork",
+    description:
+      "CrowdWork platform was developed with the mission of creating a strong community of freelancers everywhere in the world, regardless of time and distance in many different fields such as data entry.",
+    image: "",
+    type: "Website",
+    link: "https://crowdwork.dps-fpt.vn/home",
+  },
+  {
+    id: 4,
+    name: "Money Buddy",
+    description:
+      "Money Buddy is your personal financial companion designed to help you effortlessly manage your finances. Whether you are tracking your daily expenses, monitoring your income, or setting savings goals, Money Buddy makes financial management simple and intuitive. With an easy-to-use interface and powerful features, you can categorize your spending, visualize your financial habits, and gain insights into your money flow.",
+    image: "",
+    type: "CH-Play",
+    link: "https://play.google.com/store/apps/details?id=com.lenodev.studio.moneybuddy",
+  },
+  {
+    id: 5,
+    name: "Lenodev",
+    description:
+      "Lenodev is just a blog. Where I can share the technical blog for other developers.And many usefull tools I buid and annoumence in here",
+    image: "",
+    type: "Website",
+    link: "https://lenodev.com/blog",
+  },
+  {
+    id: 6,
+    name: "React Server",
+    description:
+      "React server is a website build and using only Reactjs. It is a difficult technique. Dynamic metatag serves SEO. With many years of experience research and failed. Then until now I build it success. I have to deep understanding of SPA and SSR rendering mechanism then I can do it.",
+    image: "",
+    type: "Website",
+    link: "https://react-server-lqq0.onrender.com/",
+  },
+  {
+    id: 7,
+    name: "Prismtouch",
+    description:
+      "A best performance UI library for React Native. It solve many problem of React native UI library. It is continue of building",
+    image: "",
+    type: "Package",
+    link: "https://www.npmjs.com/package/prismtouch",
+  },
+];
